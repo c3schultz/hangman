b1b473e69f310e1ab38e256299496a289d270bb3
c3schultz <c3schultz@gmail.com>
2021-07-28 19:28:14 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Actual Assignemt goes here - *save first before running*
+const promp = require("readline-sync");
+const wordBank = require("./word-bank.json");
+
+const say = prompt.question("Guess a letter:");
+
+
