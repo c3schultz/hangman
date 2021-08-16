c544c6b19351f9c6c2aa9f59a27ab894236c7f52
c3schultz <c3schultz@gmail.com>
2021-08-16 15:54:32 -0400

    hangman project update

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 //Actual Assignemt goes here - *save first before running*
-const promp = require("readline-sync");
+const prompt = require("readline-sync");
 const wordBank = require("./word-bank.json");
 
-const say = prompt.question("Guess a letter:");
 
+let select = wordBank;
+let selected = select[Math.floor(Math.random()*select.length)];
+  console.log("\nWelcome! To start type a letter.\nPress ctrl+c to stop\n");
+
+  
+
+let answerArray = [];
+for (let i = 0; i<selected.length; i++) {
+  answerArray[i] = "_";
+}
+
+let wrongGuess = !selected.length;
+let maxWrong = 6;
+
+
+let remainingLetters = selected.length;
+
+
+while (remainingLetters > 0) {
+  console.log(answerArray.join(" "));
+
+  let guess = prompt.question("Guess a letter:");
+
+  if (guess == null){
+    break;
+  }else if (guess.length !== 1) {
+    console.log("Please only guess a single letter");
+  }else {
+    for (let j = 0; j < selected.length; j++){
+      if (selected[j] === guess){
+        answerArray[j] = guess;
+        remainingLetters--; break; 
+      
+      }
+    } 
+  }
+}
+
+
+console.log(answerArray.join(" "));
+
+console.log("Good job! The answer was " + selected);
+
+ 
 
